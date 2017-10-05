28052a7b2ccdfdd4b031d643c647f7b4b6b20d91
riku1227 <rikum20021227@gmail.com>
2017-10-05 22:40:53 +0900

    Add Author card

--- app/src/main/java/jp/riku1227/mcbetool/MainActivity.kt
+++ app/src/main/java/jp/riku1227/mcbetool/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +27,14 @@ class MainActivity : AppCompatActivity() {
 
         appVersionTextView.text = res?.getString(R.string.app_version)?.format(packageInfo?.versionName)
         appVersionCodeTextView.text = res?.getString(R.string.app_version_code)?.format(packageInfo?.versionCode)
+
+        val aboutGithubButton = findViewById<Button>(R.id.about_github_button)
+        aboutGithubButton.setOnClickListener { webIntent(this,"https://github.com/riku1227/MCBETool") }
+        val aboutGooglePlusButton = findViewById<Button>(R.id.about_google_plus_button)
+        aboutGooglePlusButton.setOnClickListener { webIntent(this,"https://plus.google.com/103470090583882439463") }
+        val aboutYoutubeButton = findViewById<Button>(R.id.about_youtube_button)
+        aboutYoutubeButton.setOnClickListener { webIntent(this,"https://www.youtube.com/channel/UCIhk2bb4Y8kPCnjCeiEVtmA") }
+        val aboutHomePageButton = findViewById<Button>(R.id.about_homepage_button)
+        aboutHomePageButton.setOnClickListener { webIntent(this,"https://riku1227.github.io") }
     }
 }
