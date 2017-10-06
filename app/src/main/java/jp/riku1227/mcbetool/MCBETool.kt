15067d34ab248b6a47635a7db1a37f91fb4000dc
riku1227 <rikum20021227@gmail.com>
2017-10-06 17:04:25 +0900

    Add fun

--- app/src/main/java/jp/riku1227/mcbetool/MCBETool.kt
+++ app/src/main/java/jp/riku1227/mcbetool/MCBETool.kt
@@ -1,11 +1,23 @@
 package jp.riku1227.mcbetool
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.Toast
 
 fun webIntent(activity : Activity,urlString : String) {
     val uri = Uri.parse(urlString)
     val intent = Intent(Intent.ACTION_VIEW,uri)
     activity.startActivity(intent)
+}
+
+fun makeToast(context : Context,toastMessage : String) {
+    Toast.makeText(context,toastMessage,Toast.LENGTH_SHORT).show()
+}
+
+fun makeSnackBar(view: View,snackMessage : String) {
+    Snackbar.make(view,snackMessage,Snackbar.LENGTH_SHORT).show()
 }
