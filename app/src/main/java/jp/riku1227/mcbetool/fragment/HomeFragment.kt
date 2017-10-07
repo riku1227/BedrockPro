3f8f870304a597e463aa1d2851823f3b9c4443f3
riku1227 <rikum20021227@gmail.com>
2017-10-07 16:46:20 +0900

    Change background color

--- app/src/main/java/jp/riku1227/mcbetool/fragment/HomeFragment.kt
+++ app/src/main/java/jp/riku1227/mcbetool/fragment/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
