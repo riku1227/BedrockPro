ee224e5503784f9a3410e29291bd3c445c919dac
riku1227 <rikum20021227@gmail.com>
2017-10-10 18:51:49 +0900

    ➕ Add PermissionDialog

---
+++ app/src/main/java/jp/riku1227/mcbetool/dialog/PermissionDialog.kt
@@ -0,0 +1,29 @@
+package jp.riku1227.mcbetool.dialog
+
+import android.Manifest
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import jp.riku1227.mcbetool.R
+
+class PermissionDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val res = resources
+        return  AlertDialog.Builder(activity)
+                .setTitle(res.getString(R.string.dialog_grant_permission))
+                .setMessage(res.getString(R.string.dialog_grant_external_storage_message))
+                .setPositiveButton("OK", DialogInterface.OnClickListener { dialogInterface, i ->
+                    ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+                })
+                .setNegativeButton("Cancel",null)
+                .create()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        dismiss()
+    }
+}
