e87448dd4073708e79cec246af6b1ffd04286f42
riku1227 <rikum20021227@gmail.com>
2017-10-10 18:53:23 +0900

    ➕ Add add request permission to ResourcePackGenFragment

--- app/src/main/java/jp/riku1227/mcbetool/fragment/ResourcePackGenFragment.kt
+++ app/src/main/java/jp/riku1227/mcbetool/fragment/ResourcePackGenFragment.kt
@@ -1,14 +1,32 @@
 package jp.riku1227.mcbetool.fragment
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.DialogFragment
+import android.support.v4.content.PermissionChecker
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import jp.riku1227.mcbetool.R
+import jp.riku1227.mcbetool.dialog.PermissionDialog
+import jp.riku1227.mcbetool.util.MCBEUtil
 import kotlinx.android.synthetic.main.fragment_resource_pack_gen.*
 
 class ResourcePackGenFragment : android.support.v4.app.Fragment() {
 
+    var resourcePackName = ""
+    var resourcePackDescription = ""
+    var resourcePackHeaderUUID = ""
+    var resourcePackModuleUUID = ""
+
+    var resourcePackCache = true
+    var resourcePackAutoGenUUID = true
+    var resourcePackCustomIcon = false
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -17,5 +35,24 @@ class ResourcePackGenFragment : android.support.v4.app.Fragment() {
 
     override fun onStart() {
         super.onStart()
+
+        val pm : PackageManager = activity.packageManager
+        val mcbeUtil = MCBEUtil(pm)
+
+        if(PermissionChecker.checkSelfPermission(context,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_GRANTED) {
+            PermissionDialog().show(fragmentManager,"PermissionDialog")
+        }
+
+        resource_pack_gen_generate.setOnClickListener {
+            generateResourcePack()
+        }
+    }
+
+    private fun generateResourcePack() {
+        if(PermissionChecker.checkSelfPermission(context,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_GRANTED) {
+
+        } else {
+
+        }
     }
 }
