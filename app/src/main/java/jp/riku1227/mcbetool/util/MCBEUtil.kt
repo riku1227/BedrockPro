170d031e0422330b99caf29ae6206fcfcd32e354
riku1227 <rikum20021227@gmail.com>
2017-11-04 17:23:50 +0900

    ➕ Add editManifest to MCBEUtil

--- app/src/main/java/jp/riku1227/mcbetool/util/MCBEUtil.kt
+++ app/src/main/java/jp/riku1227/mcbetool/util/MCBEUtil.kt
@@ -3,6 +3,7 @@ package jp.riku1227.mcbetool.util
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import java.io.*
 
 class MCBEUtil(pm : PackageManager) {
 
@@ -33,6 +34,50 @@ class MCBEUtil(pm : PackageManager) {
 
     fun isInstalled() : Boolean {
         return packageInfo != null
+    }
+
+    companion object {
+        fun editManifest(filePath : String, packName : String, packDescription : String, packHeaderUUID : String, packModuleUUID : String) : String {
+            val manifestFile = File(filePath)
+            if(!manifestFile.exists()) {
+                manifestFile.createNewFile()
+            }
+            var fileContents = ""
+            var mode = 0
+            manifestFile.forEachLine {
+                val str : String
+                when {
+                    it.indexOf("\"header\"") != -1 -> {
+                        mode = 0
+                        str = it
+                    }
+                    it.indexOf("\"modules\"") != -1 -> {
+                        mode = 1
+                        str = it
+                    }
+                    it.indexOf("\"description\"") != -1 -> {
+                        if(mode == 0) {
+                            str = "        \"description\": \"$packDescription\","
+                        } else {
+                            str = "            \"description\": \"$packDescription\","
+                        }
+                    }
+                    it.indexOf("\"name\"") != -1 -> str = "        \"name\": \"$packName\","
+                    it.indexOf("\"uuid\"") != -1 -> {
+                        if(mode == 0) {
+                            str = "        \"uuid\": \"$packHeaderUUID\","
+                        } else {
+                            str = "            \"uuid\": \"$packModuleUUID\","
+                        }
+                    }
+                    else -> str = it
+                }
+                fileContents +=str + "\n"
+            }
+
+            manifestFile.writeText(fileContents)
+            return fileContents
+        }
     }
 
 }
