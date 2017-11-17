946ebfe073acdb39cfb97382659c7a9dce772886
riku1227 <rikum20021227@gmail.com>
2017-11-17 15:38:26 +0900

    ➕ Add apk backup

--- app/src/main/java/jp/riku1227/mcbetool/activity/MainActivity.kt
+++ app/src/main/java/jp/riku1227/mcbetool/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.riku1227.mcbetool.activity
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.support.annotation.RequiresApi
 import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
@@ -12,7 +13,11 @@ import android.view.MenuItem
 import jp.riku1227.mcbetool.*
 import jp.riku1227.mcbetool.fragment.HomeFragment
 import jp.riku1227.mcbetool.fragment.ResourcePackGenFragment
+import jp.riku1227.mcbetool.util.FileUtil
+import jp.riku1227.mcbetool.util.MCBEUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
@@ -47,7 +52,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         when (id) {
             R.id.drawer_home -> replaceFragment(fragmentTransaction,HomeFragment())
             R.id.drawer_resource_pack_gen -> replaceFragment(fragmentTransaction,ResourcePackGenFragment())
-            R.id.drawer_util_backup_apk -> makeToast(baseContext,"TEST")
+            R.id.drawer_util_backup_apk -> backupAPK()
         }
         drawerLayout.closeDrawers()
         return true
@@ -58,5 +63,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         fragmentTransaction.replace(R.id.flameLayout,fragment)
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
+    }
+
+    private fun backupAPK() {
+        val mcutil = MCBEUtil(packageManager)
+        val handler = Handler()
+        val outFolder = FileUtil.getExternalStoragePath() + "MCBETool/apk/"
+        val outFile = outFolder + "["+mcutil.getVersion()+"]"+"Minecraft.apk"
+        File(outFolder).mkdirs()
+        makeToast(baseContext,"Start")
+        thread {
+            FileUtil.copyFile(mcutil.getinstallLocation()!!,outFile)
+            makeThreadToast(handler,baseContext,"End")
+        }
     }
 }
