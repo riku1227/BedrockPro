87b2b0313a4831857eac195026b80192f616df0b
riku1227 <rikum20021227@gmail.com>
2017-12-01 17:22:34 +0900

    🎨 Remove unnecessary import

--- app/src/main/java/jp/riku1227/mcbetool/fragment/ResourcePackGenFragment.kt
+++ app/src/main/java/jp/riku1227/mcbetool/fragment/ResourcePackGenFragment.kt
@@ -7,7 +7,6 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
 import android.provider.Settings
 import android.support.annotation.RequiresApi
 import android.support.v4.content.PermissionChecker
