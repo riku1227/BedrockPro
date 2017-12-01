b3f1cd1d4914ca669f9237d68d55636a0cf88122
riku1227 <rikum20021227@gmail.com>
2017-12-01 16:16:33 +0900

    ➕ Add set message to ProgressDialog

--- app/src/main/java/jp/riku1227/mcbetool/dialog/ProgressDialog.kt
+++ app/src/main/java/jp/riku1227/mcbetool/dialog/ProgressDialog.kt
@@ -2,18 +2,40 @@ package jp.riku1227.mcbetool.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
+import android.widget.TextView
 import jp.riku1227.mcbetool.R
+import kotlin.concurrent.thread
 
 class ProgressDialog : DialogFragment() {
+
+    var finished = false
+    var message = ""
+
+    private var handler : Handler? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = AlertDialog.Builder(activity)
         val view = LayoutInflater.from(context).inflate(R.layout.progress_dialog, null)
+        handler = Handler()
         this.isCancelable = false
         dialog.setView(view)
         return dialog.create()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val progressText = dialog.findViewById<TextView>(R.id.progress_text)
+        thread {
+            while (!finished) {
+                handler?.post {
+                    progressText.text = message
+                }
+                Thread.sleep(100)
+            }
+        }
     }
 }
