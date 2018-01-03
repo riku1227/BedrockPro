97756e21ff8919de930f05282eb18d2710d7152b
riku1227 <rikum20021227@gmail.com>
2018-01-03 23:14:35 +0900

    ➕ Add check box function to BehaviorPackGenFragment

--- app/src/main/java/jp/riku1227/bedrockpro/fragment/BehaviorPackGenFragment.kt
+++ app/src/main/java/jp/riku1227/bedrockpro/fragment/BehaviorPackGenFragment.kt
@@ -1,10 +1,12 @@
 package jp.riku1227.bedrockpro.fragment
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import jp.riku1227.bedrockpro.R
+import kotlinx.android.synthetic.main.fragment_behavior_pack_gen.xml.*
 
 
 class BehaviorPackGenFragment : android.support.v4.app.Fragment() {
@@ -16,5 +18,27 @@ class BehaviorPackGenFragment : android.support.v4.app.Fragment() {
     override fun onStart() {
         super.onStart()
         activity?.title = "BehaviorPackGen"
+
+        behaviorPackGenAutoGenUuid.setOnClickListener {
+            if(behaviorPackGenAutoGenUuid.isChecked){
+                behaviorPackGenHeaderUuid.text = SpannableStringBuilder("")
+                behaviorPackGenModuleUuid.text = SpannableStringBuilder("")
+                behaviorPackGenHeaderUuid.visibility = View.GONE
+                behaviorPackGenModuleUuid.visibility = View.GONE
+            } else {
+                behaviorPackGenHeaderUuid.visibility = View.VISIBLE
+                behaviorPackGenModuleUuid.visibility = View.VISIBLE
+            }
+        }
+
+        behaviorPackGenCustomPackIcon.setOnClickListener {
+            if(behaviorPackGenCustomPackIcon.isChecked) {
+                behaviorPackGenPickCustomIcon.visibility = View.VISIBLE
+                behaviorPackGenCustomPackIconCard.visibility = View.VISIBLE
+            } else {
+                behaviorPackGenPickCustomIcon.visibility = View.GONE
+                behaviorPackGenCustomPackIconCard.visibility = View.GONE
+            }
+        }
     }
 }
