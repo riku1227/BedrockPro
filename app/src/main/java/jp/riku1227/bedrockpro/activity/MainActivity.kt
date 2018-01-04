2a01251c78f915079487b208bf54fd7ecdc6b19c
riku1227 <rikum20021227@gmail.com>
2018-01-04 15:25:18 +0900

    🐛 Fix double fragment

--- app/src/main/java/jp/riku1227/bedrockpro/activity/MainActivity.kt
+++ app/src/main/java/jp/riku1227/bedrockpro/activity/MainActivity.kt
@@ -26,6 +26,8 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
 
+    var centerFragmentName : String = ""
+
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,8 +47,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         if(savedInstanceState == null) {
             val homeFragment = HomeFragment()
             val fragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.flameLayout,homeFragment)
+            fragmentTransaction.replace(R.id.flameLayout,homeFragment,"HomeFragment")
             fragmentTransaction.commit()
+            centerFragmentName = "HomeFragment"
         }
     }
 
@@ -54,20 +57,23 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val id = item.itemId
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         when (id) {
-            R.id.drawer_home -> replaceFragment(fragmentTransaction,HomeFragment())
-            R.id.drawer_resource_pack_gen -> replaceFragment(fragmentTransaction,ResourcePackGenFragment())
+            R.id.drawer_home -> replaceFragment(fragmentTransaction,HomeFragment(),"HomeFragment")
+            R.id.drawer_resource_pack_gen -> replaceFragment(fragmentTransaction,ResourcePackGenFragment(),"ResourcePackGenFragment")
             R.id.drawer_util_backup_apk -> backupAPK()
-            R.id.drawer_behavior_pack_gen -> replaceFragment(fragmentTransaction,BehaviorPackGenFragment())
+            R.id.drawer_behavior_pack_gen -> replaceFragment(fragmentTransaction,BehaviorPackGenFragment(),"BehaviorPackGenFragment")
         }
         drawerLayout.closeDrawers()
         return true
     }
 
-    private fun replaceFragment(fragmentTransaction : FragmentTransaction, fragment : Fragment) {
-        fragmentTransaction.setCustomAnimations(R.anim.fade_in,R.anim.fade_out,R.anim.fade_in,R.anim.fade_out)
-        fragmentTransaction.replace(R.id.flameLayout,fragment)
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.commit()
+    private fun replaceFragment(fragmentTransaction : FragmentTransaction, fragment : Fragment, name : String) {
+        if(centerFragmentName != name) {
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in,R.anim.fade_out,R.anim.fade_in,R.anim.fade_out)
+            fragmentTransaction.replace(R.id.flameLayout,fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+            centerFragmentName = name
+        }
     }
 
     private fun backupAPK() {
