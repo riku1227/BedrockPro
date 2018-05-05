7af906b01419b27b7e9c4b4101683313fe6d2e3a
riku1227 <rikum20021227@gmail.com>
2018-05-05 17:12:38 +0900

    🔥 Remove all

--- app/src/main/java/jp/riku1227/bedrockpro/object/SubpackData.kt
+++
@@ -1,9 +0,0 @@
-package jp.riku1227.bedrockpro.`object`
-
-import java.io.Serializable
-
-data class SubpackData(
-        var name : ArrayList<String>?,
-        var directory : ArrayList<String>?,
-        var memoryTier : ArrayList<String>?
-) : Serializable
