ae57043ec7462231942a6e13b180b604b151f089
riku1227 <rikum20021227@gmail.com>
2018-05-07 18:43:05 +0900

    ➕ Add drawer

--- app/src/main/java/com/riku1227/bedrockpro/MainActivity.kt
+++ app/src/main/java/com/riku1227/bedrockpro/MainActivity.kt
@@ -2,11 +2,31 @@ package com.riku1227.bedrockpro
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(baseContext, "Unimplemented", Toast.LENGTH_LONG).show()
+        drawerLayout.closeDrawers()
+        return true
     }
 }
