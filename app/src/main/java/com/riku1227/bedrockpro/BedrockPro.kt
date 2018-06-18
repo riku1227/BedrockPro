8a3b1cf25ccb12f1572610b2f6a7af2d0abdd794
riku1227 <rikum20021227@gmail.com>
2018-06-18 17:12:41 +0900

    ➕ Add home fragment function

--- app/src/main/java/com/riku1227/bedrockpro/BedrockPro.kt
+++ app/src/main/java/com/riku1227/bedrockpro/BedrockPro.kt
@@ -1,7 +1,17 @@
 package com.riku1227.bedrockpro
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+
 class BedrockPro {
     companion object {
         const val packageName = "com.riku1227.bedrockpro"
+
+        fun webIntent(activity : Activity, urlString : String) {
+            val uri = Uri.parse(urlString)
+            val intent = Intent(Intent.ACTION_VIEW,uri)
+            activity.startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/riku1227/bedrockpro/fragment/HomeFragment.kt
+++ app/src/main/java/com/riku1227/bedrockpro/fragment/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.riku1227.bedrockpro.fragment
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.riku1227.bedrockpro.BedrockPro
 import com.riku1227.bedrockpro.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : androidx.fragment.app.Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -13,5 +16,17 @@ class HomeFragment : androidx.fragment.app.Fragment() {
 
     override fun onStart() {
         super.onStart()
+
+        val packageInfo = context?.packageManager?.getPackageInfo(BedrockPro.packageName, 0)
+        appVersion.text = resources.getString(R.string.app_version).format(packageInfo?.versionName)
+        if(Build.VERSION.SDK_INT >= 28) {
+            appVersionCode.text = resources.getString(R.string.app_version_code).format(packageInfo?.longVersionCode.toString())
+        } else {
+            appVersionCode.text = resources.getString(R.string.app_version_code).format(packageInfo?.versionCode.toString())
+        }
+
+        aboutWebsiteButton.setOnClickListener { BedrockPro.webIntent(activity!!, "https://riku1227.github.io/") }
+        aboutGithubButton.setOnClickListener { BedrockPro.webIntent(activity!!, "https://github.com/riku1227/") }
+        aboutGooglePlusButton.setOnClickListener { BedrockPro.webIntent(activity!!, "https://plus.google.com/103470090583882439463") }
     }
 }
