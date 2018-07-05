efe26c276045a215a1ac92b6944c669e4098284f
riku1227 <rikum20021227@gmail.com>
2018-07-05 15:37:16 +0900

    ➕ Add function to ui of ResourcePackGenFragment

--- app/src/main/java/com/riku1227/bedrockpro/fragment/ResourcePackGenFragment.kt
+++ app/src/main/java/com/riku1227/bedrockpro/fragment/ResourcePackGenFragment.kt
@@ -7,12 +7,53 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.riku1227.bedrockpro.R
+import kotlinx.android.synthetic.main.fragment_resource_pack_gen.*
 
 class ResourcePackGenFragment : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_resource_pack_gen, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        resourcePackGenAutoGenerateUUID.setOnClickListener {
+            if(!resourcePackGenAutoGenerateUUID.isChecked) {
+                resourcePackGenHeaderUUID.visibility = View.VISIBLE
+                resourcePackGenModuleUUID.visibility = View.VISIBLE
+            } else {
+                resourcePackGenHeaderUUID.visibility = View.GONE
+                resourcePackGenModuleUUID.visibility = View.GONE
+            }
+        }
+
+        resourcePackGenCustomPackIcon.setOnClickListener {
+            if(resourcePackGenCustomPackIcon.isChecked) {
+                resourcePackGenPickIcon.visibility = View.VISIBLE
+                resourcePackGenIconView.visibility = View.VISIBLE
+            } else {
+                resourcePackGenPickIcon.visibility = View.GONE
+                resourcePackGenIconView.visibility = View.GONE
+            }
+        }
+
+        resourcePackGenGenerate.setOnClickListener {
+
+        }
+
+        resourcePackGenPickIcon.setOnClickListener {
+
+        }
+
+        resourcePackGenSubPackEditor.setOnClickListener {
+
+        }
+
+        resourcePackGenDeleteCache.setOnClickListener {
+
+        }
     }
 
 
